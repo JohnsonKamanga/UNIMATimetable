53732d445ef8e2698173eaa8909c1f514c8fdb1b
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-06 23:35:12 +0200

    logout, settings and courses pages. no styling added yet

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,9 @@
+
+export default function Courses(){
+
+    return (
+        <div className="w-full p-2 h-full bg-[#F8F7F7] rounded-md">
+            courses
+        </div>
+    )
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,9 @@
+
+export default function Settings(){
+
+    return (
+        <div className="w-full p-2 min-h-lvh bg-[#F8F7F7] rounded-md">
+            Settings
+        </div>
+    )
+}
