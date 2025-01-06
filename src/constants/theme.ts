b54d1c6f554e10714fa525c554953cc50c621132
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-06 22:40:36 +0200

    the theme file contains objects that specify how the UI should look depending on the theme mode

---
+++ src/constants/theme.ts
@@ -0,0 +1,13 @@
+
+export const Theme = {
+    light: {
+        sideBarColor: '003049',
+        backgroundColor: '#F8F7F7',
+        textColor: '#ffffff'
+    },
+    dark: {
+        sideBarColor: '003049',
+        backgroundColor: '#F8F7F7',
+        textColor: '#ffffff'
+    }
+}
