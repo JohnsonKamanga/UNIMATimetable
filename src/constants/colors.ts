3537decc9c55d730b6289fcb7c552e40c78dd608
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-06 22:40:54 +0200

    a file containing colors used in the system

---
+++ src/constants/colors.ts
@@ -0,0 +1,13 @@
+/**
+ * red: #D62828
+ * orange: #F77F00
+ * yellow: #FCBF49
+ * pale yellow: #EAE2B7
+ */
+
+export const Colors = {
+    red: '#D62828',
+    orange: '#F77F00',
+    yellow: '#FCBF49',
+    paleYellow: '#EAE2B7',
+}
