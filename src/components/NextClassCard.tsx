6a78665b78a5f73d9874fee1d928b421b2c5c9d1
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-15 12:06:49 +0200

    created a card for displaying the next class

---
+++ src/components/NextClassCard.tsx
@@ -0,0 +1,120 @@
+import { isFriday, isWeekend, nextMonday, startOfTomorrow } from "date-fns";
+import { BookOpen, Building2, Clock4 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { getPeriodStartTime, getPeriodTime } from "../functions/time";
+
+export default function NextClassCard({
+  code,
+  venue,
+  time,
+  lastClassTime,
+}: {
+  code: string;
+  venue: string;
+  time: string;
+  lastClassTime: string | undefined;
+}) {
+  const getNextClassTime = (time: string): Date => {
+    const slot = time[time.length - 1];
+    const start = getPeriodStartTime(slot);
+
+    if (isFriday(Date.now()) || isWeekend(Date.now())) {
+      const nextMon = nextMonday(Date.now());
+      nextMon.setHours(
+        Number(start[0] + start[1]),
+        Number(start[3] + start[4]),
+        0
+      );
+      return nextMon;
+    }
+
+    //if the last class is in the same day as the next class
+    if (
+      lastClassTime &&
+      ((Number(lastClassTime) < 10 && Number(time) < 10) ||
+        lastClassTime[0] === time[0])
+    ) {
+      const today = new Date(Date.now());
+      today.setHours(
+        Number(start[0] + start[1]),
+        Number(start[3] + start[4]),
+        0
+      );
+      return today;
+    }
+
+    const nextDay = startOfTomorrow();
+    nextDay.setHours(
+      Number(start[0] + start[1]),
+      Number(start[3] + start[4]),
+      0
+    );
+    return nextDay;
+  };
+
+  const calculateRemainingTime = (time: string) => {
+    const nextClass = getNextClassTime(time);
+    const delta = nextClass.getTime() - Date.now();
+    const hours = Math.floor(delta / 3600000);
+    const rem1 = delta - hours * 3600000;
+    const mins = Math.floor(rem1 / 60000);
+    const rem2 = rem1 - mins * 60000;
+    const secs = Math.floor(rem2 / 1000);
+    return (
+      (hours < 10 ? "0" + hours : hours) +
+      ":" +
+      (mins < 10 ? "0" + mins : mins) +
+      ":" +
+      (secs < 10 ? "0" + secs : secs)
+    );
+  };
+  const [remTime, setRemTime] = useState("");
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setRemTime(() => calculateRemainingTime(time));
+    }, 1000);
+
+    return () => clearInterval(timeout);
+  }, [remTime]);
+
+  return (
+    <div className="flex flex-row gap-x-2 border-[1px] bg-[#EAE2B7] border-black w-fit p-2 rounded-xl border-opacity-20 shadow-lg">
+      <div className="flex flex-col min-w-[160px] gap-y-1">
+        <div className="flex items-center gap-x-2 p-[9px] bg-black bg-opacity-20 w-fit border-black border-[1px] border-opacity-30 shadow-lg rounded-3xl">
+          <div className="border-[2px] border-black rounded-full p-1 flex items-center justify-center">
+            <BookOpen size={16} />
+          </div>
+          <p className="font-bold">{code}</p>
+        </div>
+        <div className="flex items-center gap-x-2 p-[9px] bg-black bg-opacity-20 w-fit border-black border-[1px] border-opacity-30 shadow-lg rounded-3xl">
+          <div className="border-[2px] border-black rounded-full p-1 flex items-center justify-center">
+            <Building2 size={16} />
+          </div>
+          <p className="font-bold">{venue}</p>
+        </div>
+        <div className="flex items-center gap-x-2 p-[9px] bg-black bg-opacity-20 w-fit border-black border-[1px] border-opacity-30 shadow-lg rounded-3xl">
+          <div className="border-[2px] border-black rounded-full p-1 flex items-center justify-center">
+            <Clock4 size={16} />
+          </div>
+          <p className="font-bold">{getPeriodTime(time[time.length - 1])}</p>
+        </div>
+      </div>
+      <div className="w-[145px] text-center aspect-square rounded-lg bg-black bg-opacity-40 border-[1px] border-black border-opacity-25 shadow-md">
+        <div className="font-bold">Time until next class</div>
+        <div className="font-semibold text-3xl">
+          {remTime[0]}
+          {remTime[1]}
+        </div>
+        <div className="font-medium text-2xl">
+          {remTime[3]}
+          {remTime[4]}
+        </div>
+        <div className="font-normal text-xl">
+          {remTime[6]}
+          {remTime[7]}
+        </div>
+      </div>
+    </div>
+  );
+}
