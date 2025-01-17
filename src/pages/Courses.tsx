fa25e4f10f0facea5fc1eddb036daf22da01a67c
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-17 14:48:59 +0200

    added data fetching from the backend api and styled the page using the course card

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,9 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { baseurl } from "../constants/url";
+import Loader from "../components/Loader";
+import { CourseCard, TCourseInfo } from "../components/CourseCard";
 
 export default function Courses(){
+    const user = {
+        id: 1
+    }
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const drawCourses = (courseInfo: TCourseInfo, index: number)=>{
+        return <CourseCard key={index} courseInfo={courseInfo}/>
+    }
+
+    useEffect(()=>{
+        axios.get(`${baseurl}/timetable/view/current/courses?userid=${user.id}`)
+        .then((timetable)=>{
+            setCourses(timetable.data);
+            setLoading(false);
+        })
+        .catch((err)=>{
+            console.error('An error occured while fetching your courses: ', err)
+        })
+    },[]);
+
+    if (loading) {
+        return (
+          <div className="w-full flex flex-grow">
+            <div className="h-full w-full flex items-center justify-center bg-black bg-opacity-25">
+              <Loader message="Fetching Courses" />
+            </div>
+          </div>
+        );
+      }
 
     return (
-        <div className="w-full p-2 h-full bg-[#F8F7F7] rounded-md">
-            courses
+        <div className="flex flex-grow flex-col bg-[#F8F7F7]">
+        <div className="p-2 min-h-full gap-2 bg-[#F8F7F7] rounded-md grid grid-cols-3">
+            {courses.map(drawCourses)}
+        </div>
         </div>
     )
 }
