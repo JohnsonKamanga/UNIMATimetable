28e3c08f85186908ec97722ed0445ea9303131e8
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-25 22:09:34 +0200

    updated the settings navbar navlink paths

--- src/components/SettingsNavBar.tsx
+++ src/components/SettingsNavBar.tsx
@@ -1,31 +1,32 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 
 export default function SettingsNavBar() {
+  const path = useLocation();
   return (
     <>
-    <h1 className="font-semibold text-3xl">Settings</h1>
-    <div className="flex flex-row">
-      <NavLink
-        to="/settings/account-info/johnson_kamanga"
-        className={({ isActive }) =>
-          isActive
-            ? "p-2 text-xl hover:bg-black hover:bg-opacity-5 rounded-md hover:shadow-md font-normal text-black"
-            : "p-2 text-xl hover:bg-black hover:bg-opacity-5 rounded-md hover:shadow-md font-normal text-opacity-35 text-black"
-        }
-      >
-        <p>Account Info</p>
-      </NavLink>
-      <NavLink
-        to="/settings/appearance"
-        className={({ isActive }) =>
-          isActive
-            ? "p-2 text-xl hover:bg-black hover:bg-opacity-5 rounded-md hover:shadow-md font-normal text-black"
-            : "p-2 text-xl hover:bg-black hover:bg-opacity-5 rounded-md hover:shadow-md font-normal text-opacity-35 text-black"
-        }
-      >
-        <p>Appearance</p>
-      </NavLink>
-    </div>
+      <h1 className="font-semibold text-3xl mb-2">Settings</h1>
+      <div className="flex flex-row gap-x-2 p-1">
+        <NavLink
+          to="/settings"
+          className={({ isActive }) =>
+            (isActive && path.pathname === "/settings")
+              ? "p-1 text-lg hover:bg-black hover:bg-opacity-5 rounded-sm hover:shadow-md font-medium text-black"
+              : "p-1 text-lg hover:bg-black hover:bg-opacity-5 rounded-sm hover:shadow-md font-medium text-opacity-35 text-black"
+          }
+        >
+          <p>Preferances</p>
+        </NavLink>
+        <NavLink
+          to="/settings/account-info/johnson_kamanga"
+          className={({ isActive }) =>
+            isActive
+              ? "p-1 text-lg hover:bg-black hover:bg-opacity-5 rounded-sm hover:shadow-md font-medium text-black"
+              : "p-1 text-lg hover:bg-black hover:bg-opacity-5 rounded-sm hover:shadow-md font-medium text-opacity-35 text-black"
+          }
+        >
+          <p>Account</p>
+        </NavLink>
+      </div>
     </>
   );
 }
