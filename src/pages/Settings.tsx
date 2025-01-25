85db4977e618d4355a666d3d1099b360b6905ff9
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-25 22:10:23 +0200

    added an outlet to the settings page to render child routes

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router";
+import SettingsNavBar from "../components/SettingsNavBar";
 
-export default function Settings(){
-
-    return (
-        <div className="w-full p-2 min-h-lvh bg-[#F8F7F7] rounded-md">
-            Settings
-        </div>
-    )
+export default function Settings() {
+  return (
+    <div className="w-full p-8 min-h-lvh bg-[#F8F7F7] rounded-md">
+      <SettingsNavBar />
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
