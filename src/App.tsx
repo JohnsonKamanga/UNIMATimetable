93edb9a9abbc18e66c71ea9becaa094b7a1e65fe
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-30 23:50:12 +0200

    added a new landing page route and configured the user context

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,37 @@ import TimeTables from "./pages/TimeTables";
 import AccountInfo from "./pages/AccountInfo";
 import Appearance from "./pages/Appearance";
 import Dashboard from "./pages/Dashboard";
+import Landing from "./pages/Landing";
+import { UserContext } from "./user-context";
+import { useUser } from "./hooks/useUser";
 
 function App() {
+  const { user, setUser } = useUser();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route index element={<Home />} />
-          <Route path="timetable">
-            <Route index element={<TimeTables />} />
-            <Route path=":name" element={<Timetable />} />
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="timetable">
+              <Route index element={<TimeTables />} />
+              <Route path=":name" element={<Timetable />} />
+            </Route>
+            <Route path="courses" element={<Courses />} />
+            <Route path="settings" element={<Settings />}>
+              <Route index element={<Appearance />} />
+              <Route path="account-info/:username" element={<AccountInfo />} />
+            </Route>
           </Route>
-          <Route path="courses" element={<Courses />} />
-          <Route path="settings" element={<Settings />}>
-            <Route index element={<Appearance />} />
-            <Route path="account-info/:username" element={<AccountInfo />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
